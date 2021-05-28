2ef7ab4a7830efe1c5dc920099e36c7bfd35c1b1
Nam Huynh Nhat <nnhuynh306@gmail.com>
2021-05-28 10:34:27 +0700

    + add tab layout

---
+++ src/app/src/main/java/vn/edu/hcmus/fit/mssv18127014_18127208/map/Views/Fragments/DataListFragment.java
@@ -0,0 +1,27 @@
+package vn.edu.hcmus.fit.mssv18127014_18127208.map.Views.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import vn.edu.hcmus.fit.mssv18127014_18127208.map.R;
+
+public class DataListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_data_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
